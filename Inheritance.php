190b25a1998bde3054b780a6f995e684c04de8ea
Alissa Wiley <alissa.estelle.w@gmail.com>
2023-03-16 13:47:22 -0400

    Successfully Implemented => Fx

--- Inheritance.php
+++ Inheritance.php
@@ -2,16 +2,20 @@
 
 // Water Bottle Example
 
-class Nalgene {
-  function fill() {
+class Nalgene
+{
+  function fill()
+  {
     print "Filling with Water!";
     print "\n";
   }
 }
 
-class HydroFlask extends Nalgene {
+class HydroFlask extends Nalgene
+{
   // HydroFlask is a 
-  function chill() {
+  function chill()
+  {
     print "Keeping Water Cold!";
     print "\n";
   }
@@ -22,35 +26,44 @@ class HydroFlask extends Nalgene {
 
 // Collection Example
 
-class Collection {
+class Collection
+{
   function __construct(public $collectibles)
-  {}
-}
-
-class Book {
-  function __construct(
-    public $title, 
-    public $author,
-    public $price)
-  {}
-
-  // function getTitle() =>
-  // $this->title;
+  {
+  }
 
   // **Array Map Goes Here**
-  function total() {
-    array_map(fn ($collectible) => $collectible->price, $this->collectibles);
+  function sumPrices()
+  {
+    $total = array_map(fn($collectible) => $collectible->price, $this->collectibles);
+    return array_sum($total);
   }
+}
+
+class Book
+{
+  function __construct(
+    public $title,
+    public $author,
+    public $price
+  )
+  {
+  }
+
+// function getTitle() =>
+// $this->title;
+
 }
 
 $collection = new Collection([
   $thicNhat = new Book('Living Buddha, Living Christ', 'Thic Nhat Hanh', 15),
   // i.e. new Book($title => "Living Buddha, Living Christ", $author => "Thic Nhat Hanh", $price => 15)
   $branSan = new Book('The Way of Kings', 'Brandon Sanderson', 20)
-    // i.e. new Book($title => "The Way of Kings", $author => "Brandon Sanderson", $price => 20)
+  // i.e. new Book($title => "The Way of Kings", $author => "Brandon Sanderson", $price => 20)
 ]);
 
 var_dump($collection);
-var_dump($thicNhat)
+// var_dump($thicNhat)
+print($collection->sumPrices())
 
-?>
+  ?>
