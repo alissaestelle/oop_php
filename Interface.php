3cccb552835dbb2cfb212256dc368d665d79c4c0
Alissa Wiley <alissa.estelle.w@gmail.com>
2023-03-20 09:14:53 -0400

    Changing Machines

--- Interface.php
+++ Interface.php
@@ -1,1 +1,24 @@
-<?php ?>
+<?php
+
+class Newsletter
+{
+  function __construct(public $aPI)
+  {
+  }
+  function subscribe($email)
+  {
+    // Sends Post Req to Newsletter's API
+  }
+}
+
+class SubscriptionCT
+{
+  function register()
+  {
+    // Adds User Email to Newsletter List
+    $subscription = new Newsletter();
+    $subscription->subscribe();
+  }
+}
+
+?>
